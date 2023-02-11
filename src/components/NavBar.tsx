f2a67e65e755f26e2c99f700f220888efa5f9b8d
athulgeorge37 <athulgeorge37@gmail.com>
2023-02-11 15:58:14 +1100

    ads NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,71 @@
+"use client";
+// context
+
+// hooks
+import React, { useState } from "react";
+
+// custom hooks
+
+// features
+
+// form
+
+// ui
+import { Bars3Icon } from "@heroicons/react/24/solid";
+
+// animations
+import { Transition } from "@headlessui/react";
+
+// helper
+
+interface NavBarProps {}
+
+const NavBar = ({}: NavBarProps) => {
+    const [navbarIsOpen, setNavbarIsOpen] = useState(false);
+
+    return (
+        <nav id="NavBar" className="mx-auto w-full max-w-5xl">
+            <div className="flex items-center justify-between p-4">
+                <div>Logo</div>
+                <ul className="hidden gap-5 sm:flex">
+                    <li>Resume</li>
+                    <li>Projects</li>
+                    <li>Work</li>
+                    <li>Contact</li>
+                    <li>Linked In</li>
+                    <li>GitHub</li>
+                </ul>
+
+                <button
+                    className="p-2 sm:hidden"
+                    onClick={() => setNavbarIsOpen(!navbarIsOpen)}
+                >
+                    <Bars3Icon className="h-5 w-5" />
+                </button>
+            </div>
+
+            <Transition
+                className="z-100 fixed top-0 left-0 flex h-screen w-full max-w-xs flex-col gap-10 bg-blue-200 p-5 sm:hidden"
+                show={navbarIsOpen}
+                enter="transform transition ease-in-out duration-500 "
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 "
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+            >
+                <div>Logo</div>
+                <ul className="flex flex-col gap-5">
+                    <li>Resume</li>
+                    <li>Projects</li>
+                    <li>Work</li>
+                    <li>Contact</li>
+                    <li>Linked In</li>
+                    <li>GitHub</li>
+                </ul>
+            </Transition>
+        </nav>
+    );
+};
+
+export default NavBar;
